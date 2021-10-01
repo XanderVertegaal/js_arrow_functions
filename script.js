d5aa5d6b82967047245852afe34ca086afbf0858
Xander Vertegaal <xander.vertegaal@xs4all.nl>
2021-10-01 11:37:59 +0200

    Arrow function exercises done

---
+++ script.js
@@ -0,0 +1,14 @@
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!")
+ikRockArrowFunctions()
+
+const fivePlusSeven = () => 5 + 7
+console.log(fivePlusSeven())
+
+const myFunction = (a,b) => a + b
+console.log(myFunction(5,6))
+
+const addFive = a => a + 5
+console.log(addFive(2))
+
+const createObject = () => ({greeting: 'hoi'})
+console.log(createObject)
